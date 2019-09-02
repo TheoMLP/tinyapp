ecc517ced9f5f1edb6f1373cf07a37c0e82dd737
TheoMLP <theomalaper.cognez@gmail.com>
2019-09-02 22:38:27 +0000

    added short URLs to urldatabase redirect after form submission redirect short URL's

--- express_server.js
+++ express_server.js
@@ -35,14 +35,20 @@ app.get('/urls/new', (req, res) => {
 })
 
 app.post('/urls', (req, res) => {
-  console.log(req.body)
-  res.send('ok')
+  let shortURL = generateRandomString()
+  urlDatabase[shortURL] = req.body.longURL
+  res.redirect(`/urls/${shortURL}`)
 })
 
 app.get('/urls/:shortURL', (req, res) => {
   let templateURL = { shortURL: req.params.shortURL, longURL: urlDatabase[req.params.shortURL]}
   res.render('urls_show', templateURL)
 })
+
+app.get("/u/:shortURL", (req, res) => {
+  const longURL = urlDatabase[req.params.shortURL]
+  res.redirect(longURL);
+});
 
 app.get('/hello', (req, res) => {
   res.send('<html><body>Hello <b>World</b></body></html>\n')
