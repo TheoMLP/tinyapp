cb2ff198d64c4ad27c72fb962bfacaa36387b8f6
TheoMLP <theomalaper.cognez@gmail.com>
2019-09-02 21:21:20 +0000

    added urls/new routing

--- express_server.js
+++ express_server.js
@@ -23,6 +23,10 @@ app.get('/urls', (req, res) => {
   res.render('urls_index', templateUrls)
 })
 
+app.get('/urls/new', (req, res) => {
+  res.render('urls_new')
+})
+
 app.get('/urls/:shortURL', (req, res) => {
   let templateURL = { shortURL: req.params.shortURL, longURL: urlDatabase[req.params.shortURL]}
   res.render('urls_show', templateURL)
