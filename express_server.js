9a3cfcf51546c97596827d94399b7f8623b8b8de
TheoMLP <theomalaper.cognez@gmail.com>
2019-09-03 16:56:45 +0000

    added delete button post request to delete url in databse and redirection

--- express_server.js
+++ express_server.js
@@ -45,10 +45,16 @@ app.get('/urls/:shortURL', (req, res) => {
   res.render('urls_show', templateURL)
 })
 
-app.get("/u/:shortURL", (req, res) => {
+app.get('/u/:shortURL', (req, res) => {
   const longURL = urlDatabase[req.params.shortURL]
   res.redirect(longURL);
 });
+
+app.post('/urls/:shortURL/delete', (req, res)  => {
+  const shortURL = req.params.shortURL
+  delete urlDatabase[shortURL]
+  res.redirect('/urls')
+})
 
 app.get('/hello', (req, res) => {
   res.send('<html><body>Hello <b>World</b></body></html>\n')
