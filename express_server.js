576609addb66e4d2a7fe4ce396c36e47d963b3d7
TheoMLP <theomalaper.cognez@gmail.com>
2019-09-04 21:11:08 +0000

    implemented code so that only registered users can shorten URls

--- express_server.js
+++ express_server.js
@@ -88,7 +88,11 @@ app.get('/urls', (req, res) => {
 
 app.get('/urls/new', (req, res) => {
   let templateVars = { user: users[req.cookies["user_id"]]}
-  res.render('urls_new', templateVars)
+  if (templateVars.user) {
+    res.render('urls_new', templateVars)
+  } else {
+    res.redirect('/login')
+  }
 })
 
 app.post('/urls', (req, res) => {
