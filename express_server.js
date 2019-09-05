4ea722c311f90cc7fe0b8ba538ae9d64913d90a8
TheoMLP <theomalaper.cognez@gmail.com>
2019-09-05 16:20:04 +0000

    fixed style of login register and main page

--- express_server.js
+++ express_server.js
@@ -1,7 +1,7 @@
 const express = require('express');
 const bodyParser = require('body-parser');
 const cookieParser = require('cookie-parser');
-const bcrypt = require('bcrypt')
+const bcrypt = require('bcrypt');
 const app = express();
 const port = 8080;
 
